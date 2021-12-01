d1eb8f457729ac5c440de2f353d67f5a9f541313
Eliot Partridge <byte@code.horse>
2021-12-01 11:01:29 -0600

    Clean up init script

--- init-day.sh
+++ init-day.sh
@@ -8,11 +8,9 @@ if [ "$#" -ne 1 ]; then
 fi
 
 DAYNO="$1"
-
-DAYDIR="Day $(printf %02d $DAYNO)"
+DAYDIR="Day $(printf %02d "$DAYNO")"
 
 mkdir "$DAYDIR"
-
 cp -r .template/* "$DAYDIR"
 
 echo "$DAYDIR created. GO GO GO!"
