1bdd8745e6629ad70bc5831ab8fe0447c2d2b4b3
Eliot Partridge <byte@code.horse>
2021-12-01 13:17:50 -0600

    ... why did I do it that way?

--- Day 01/part1.py
+++ Day 01/part1.py
@@ -8,7 +8,7 @@ lines = [int(line.strip()) for line in lines if line != '']
 inc = 0
 
 for i in range(len(lines) - 1):
-	if lines[i+1] - lines[i] > 0:
+	if lines[i+1] > lines[i]:
 		inc += 1
 
 print(inc)
