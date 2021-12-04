cd1fbe16db00b9a3016c199d5f5387ed2d6afe9d
Eliot Partridge <byte@code.horse>
2021-12-03 23:19:04 -0600

    Complete day 04

---
+++ Day 04/part2.py
@@ -0,0 +1,63 @@
+import sys
+from typing import List
+
+MARK_CH = 'X'
+
+with open(sys.argv[1]) as f:
+	lines = [line.strip() for line in f.readlines()]
+
+def is_bingo_winner(board: List[List[int]]):
+	for row in board:
+		if row[0] == row[1] == row[2] == row[3] == row[4] == MARK_CH:
+			return True
+	for col in range(5):
+		if board[0][col] == board[1][col] == board[2][col] == board[3][col] == board[4][col] == MARK_CH:
+			return True
+	return False
+
+def print_board(board: List[List[int]]):
+	for row in board:
+		print(' '.join(str(n) for n in row))
+	print()
+
+def score_board(board: List[List[int]]):
+	return sum(sum(n for n in row if n != MARK_CH) for row in board)
+
+drawing = [int(n) for n in lines[0].split(',')]
+
+boards = []
+
+current_board = []
+for line in lines[2:]:
+	if line == '':
+		boards.append(current_board)
+		current_board = []
+	else:
+		current_board.append([int(n) for n in line.split()])
+
+boards.append(current_board)
+
+already_winners = []
+winning_board = None
+winning_draw = None
+
+for draw in drawing:
+	print(draw)
+	for board in boards:
+		if board in already_winners: continue
+		for row in board:
+			for i, n in enumerate(row):
+				if n == draw:
+					row[i] = MARK_CH
+		if is_bingo_winner(board):
+			unmarked_nums_sum = score_board(board)
+			print(unmarked_nums_sum)
+			print(draw)
+			print(unmarked_nums_sum * draw)
+			if board not in already_winners:
+				winning_board = board
+				already_winners.append(board)
+				winning_draw = draw
+
+print_board(winning_board)
+print(score_board(winning_board) * winning_draw)
